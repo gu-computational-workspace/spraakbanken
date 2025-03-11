ef010af78083019be586df83014a78f8024e7b22
xcasra <ragnar.castillo@gu.se>
2025-03-11 21:20:34 +0100

    minor changes

--- strix-sentence-transformers/kuberay/run_ray_transformers.py
+++ strix-sentence-transformers/kuberay/run_ray_transformers.py
@@ -1,8 +1,15 @@
+import sys
 from ray.job_submission import JobSubmissionClient
+
+runtime_env = {
+                "pip": "./requirements.txt",
+                "working_dir": "./"
+        }
 
 client = JobSubmissionClient("http://127.0.0.1:8265")
 job_id = client.submit_job(
-    entrypoint="python main.py",
-    runtime_env={"working_dir": "./"}
+    entrypoint="python main.py " + sys.argv[1],
+    runtime_env=runtime_env
 )
+
 print(job_id)
